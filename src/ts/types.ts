c605b8f0d8b141d3ce55c9df8760ec02a40f683b
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-06-05 11:51:22 +0200

    definition + structur

---
+++ src/ts/types.ts
@@ -0,0 +1,33 @@
+export type Theme = 'code-logos' | 'games' | 'da-projects' | 'food';
+
+export type BoardSize =  '4x4' | '5x4' | '6x6';
+
+export interface Player {
+    name: string;
+    score: number;
+    color: 'orange' | 'blue';
+}
+
+export interface CardDate {
+    id: number;
+    pairID: number;
+    theme: Theme;
+    imageUrl: string;
+    isFlipped: boolean;
+    isMatched: boolean;
+}
+
+export interface Gamesettings{
+    theme: Theme;
+    boardSize: BoardSize;
+    players: [Player, Player];
+}
+
+export interface GameState {
+    settings: Gamesettings;
+    cards: CardData[];
+    currentPlayerIndex: 0 | 1;
+    flippedCards: CardData[];
+    isLocked: boolean;
+    moves: number; 
+}
