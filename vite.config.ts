ae3f5aeb27bfd5409daaa0015c23e6ab0f320159
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-06-05 14:55:47 +0200

    add font + colors variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    css: {
-        
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "/src/styles/variables" as *;`
+      }
     }
+  }
 })
