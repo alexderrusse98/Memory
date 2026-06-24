8ee2b504bd3d1a5c6681bd43968af07acbc74422
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-06-24 19:39:42 +0200

    change preview img

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import './styles/style.scss';
+
+// Alle Theme-Radios greifen (die mit name="theme")
+const themeRadios = document.querySelectorAll('input[name="theme"]');
+
+// Alle Vorschaubilder greifen
+const previewImages = document.querySelectorAll('.settings__preview-img');
+
+console.log(themeRadios);
+console.log(previewImages);
+
+themeRadios.forEach((radio) => {
+    radio.addEventListener('change', () => {
+        // alle verstecken
+        previewImages.forEach((img) => {
+            img.setAttribute('hidden', '');
+        });
+
+        // passendes zeigen
+        const selectedTheme = (radio as HTMLInputElement).value;
+        const activeImg = document.querySelector(`.settings__preview-img[data-theme="${selectedTheme}"]`);
+        activeImg?.removeAttribute('hidden');
+    });
+});
