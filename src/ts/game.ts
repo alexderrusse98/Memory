5214ad4907b5ad402af783ec029e6f78ff40b52e
alexderrusse98 <alex.weiswasser.com@gmail.com>
2026-07-02 22:12:43 +0200

    add icon list

---
+++ src/ts/game.ts
@@ -0,0 +1,5 @@
+import { gameSettings } from './state';
+
+
+const cardCount = Number(gameSettings.boardSize);
+const pairCount = cardCount / 2;
---
+++ src/ts/themeIcon.ts
@@ -0,0 +1,17 @@
+import type { Theme } from './types';
+
+export const themeIcons: Record<Theme, string[]> = {
+    'code-vibes': [
+        'angular-icon.svg', 'bootstrap-icon.svg', 'claudflare-icon.svg', 'css-icon.svg',
+        'dj-icon.svg', 'firebase-icon.svg', 'git-hub-icon.svg', 'git-icon.svg', 'html5-icon.svg',
+        'js-icon.svg', 'python-icon.svg', 'react-icon.svg', 'sass-icon.svg', 'terminal-icon.svg',
+        'ts-icon.svg', 'vs-code-icon.svg', 'vsue-icon.svg', 'node-icon.svg'
+    ],
+    'food': [
+        'brezel.svg', 'burger.svg', 'cake.svg', 'cake2.svg', 'chicken.svg', 'cookies.svg',
+        'corndog.svg', 'donut.svg', 'frites.svg', 'ice.svg', 'muffin.svg', 'pizza.svg',
+        'salad.svg', 'sandwich.svg', 'schoko.svg', 'sushi.svg', 'tacco.svg', 'wrap.svg'],
+
+    'games': [],
+    'da-projects': [],
+}
